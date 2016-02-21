e3bf4b43c8c7fb1ee82f4ef710ab2d6c0c9ff48e
David Lougheed <david.lougheed@gmail.com>
2016-02-21 05:11:52 -0500

    Can add members to chat

--- controllers/conversations.js
+++ controllers/conversations.js
@@ -12,7 +12,6 @@ module.exports.controller = function (objects) {
 		if(!req.isAuthenticated()) {
 			return res.send('not authenticated'); // TODO: Handle non authentication
 		}
-
 		if (!req.body) {
 			return res.send('error no body');
 		}
@@ -70,6 +69,9 @@ module.exports.controller = function (objects) {
 		if(!req.isAuthenticated()) {
 			return res.send('not authenticated'); // TODO: Handle non authentication
 		}
+		if (!req.body) {
+			return res.send('error no body');
+		}
 
 		objects.models.Conversation.findOne({where: {
 				id: req.params.id
@@ -79,6 +81,35 @@ module.exports.controller = function (objects) {
 			conversation.save();
 
 			return res.send({ success: true });
+		});
+	});
+	objects.router.put('/conversation_users/:id/', function (req, res) {
+		res.setHeader('Content-Type', 'application/json');
+
+		if(!req.isAuthenticated()) {
+			return res.send('not authenticated'); // TODO: Handle non authentication
+		}
+		if (!req.body) {
+			return res.send('error no body');
+		}
+
+		objects.models.Conversation.findOne({where: {
+			id: req.params.id
+		}
+		}).then(function (conversation) {
+			objects.models.User.findOne({
+				where: {
+					username: req.body.username
+				}
+			}).then(function (user) {
+				if(user) {
+					conversation.addUser(user);
+
+					return res.send({success: true});
+				} else {
+					return res.send({success: false});
+				}
+			});
 		});
 	});
 	objects.router.delete('/conversation/:id/', function (req, res) {
