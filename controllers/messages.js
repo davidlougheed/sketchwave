df26b3e25067278fa8abc88a1c166dfce2d54d4f
David Lougheed <david.lougheed@gmail.com>
2016-03-10 18:20:52 -0500

    Add some checks for message sender being part of conversation, add some join/leave conversation handling, redo homepage with quotes, some tweaks to signup page.

--- controllers/messages.js
+++ controllers/messages.js
@@ -32,31 +32,78 @@ module.exports.controller = function (objects) {
 
 	objects.io.on('connection', function (socket) {
 		socket.on('addUser', function (data) {
-			// TODO: Send user connect message, handle online stuff...
+			// TODO: Send user connect message, handle online/offline stuff...
+			// TODO: Cache conversation data FOR ALL APP!!! SO THAT NOT SO MANY DB REQUESTS ARE MADE
 
-			socket.join('conversation' + data.conversation.toString());
+			objects.models.Conversation.findOne({
+				where: {
+					id: parseInt(data.conversationID)
+				}
+			}).then(function (conversation) {
+				conversation.getUsers({
+					where: {
+						id: socket.request.session.passport.user
+					}
+				}).then(function (users) {
+					if (users != null && users.length > 0) {
+						socket.join('conversation' + data.conversationID.toString());
+
+						socket.broadcast.to('conversation' + data.conversationID.toString())
+							.emit('addUser', users[0].username);
+					}
+				});
+			});
+		});
+		socket.on('removeUser', function (data) {
+			// TODO: Send user disconnect message, handle online/offline stuff...
+
+			socket.leave('conversation' + data.conversationID.toString());
+
+			objects.models.Conversation.findOne({
+				where: {
+					id: parseInt(data.conversationID)
+				}
+			}).then(function (conversation) {
+				conversation.getUsers({
+					where: {
+						id: socket.request.session.passport.user
+					}
+				}).then(function (users) {
+					if (users != null && users.length > 0) {
+						socket.broadcast.to('conversation' + data.conversationID.toString())
+							.emit('removeUser', users[0].username);
+					}
+				});
+			});
 		});
 
 		socket.on('newMessage', function (data) {
 			// If user is properly signed in
-			// TODO: Check user is part of conversation that conversationID specifies
 
 			if (socket.request.session.passport.user) {
-				objects.models.Message.create({
-					imageData: [data.imageData],
-					ConversationId: parseInt(data.conversationID),
-					UserId: parseInt(socket.request.session.passport.user)
-				}).then(function (message) {
-					objects.models.Conversation.findOne({
+				objects.models.Conversation.findOne({
+					where: {
+						id: parseInt(data.conversationID)
+					}
+				}).then(function (conversation) {
+					conversation.getUsers({
 						where: {
-							id: parseInt(data.conversationID)
+							id: socket.request.session.passport.user
 						}
-					}).then(function (conversation) {
-						conversation.lastMessage = Date.now();
-						conversation.save();
+					}).then(function (users) {
+						if (users != null && users.length > 0) {
+							objects.models.Message.create({
+								imageData: [data.imageData],
+								ConversationId: parseInt(data.conversationID),
+								UserId: parseInt(socket.request.session.passport.user)
+							}).then(function (message) {
+								conversation.lastMessage = Date.now();
+								conversation.save();
 
-						socket.broadcast.to('conversation' + data.conversationID.toString())
-							.emit('newMessage', message.toJSON());
+								socket.broadcast.to('conversation' + data.conversationID.toString())
+									.emit('newMessage', message.toJSON());
+							});
+						}
 					});
 				});
 			} else {
