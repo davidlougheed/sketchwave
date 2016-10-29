1f46eab1d246142a797b764a9495f2a29a8cd189
David Lougheed <david.lougheed@gmail.com>
2016-10-29 14:45:29 -0400

    Bounce some message logic into app-message module.

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var sanitizeHtml = require('sanitize-html');
+var appMessage = require('../modules/app-message');
 
 module.exports.controller = function (objects) {
 	objects.io.on('connection', function (socket) {
@@ -8,52 +8,7 @@ module.exports.controller = function (objects) {
 			// If user is properly signed in
 
 			if (socket.request.session.passport.user) {
-				objects.models.Conversation.findOne({
-					where: {
-						id: parseInt(data.conversationID)
-					}
-				}).then(function (conversation) {
-					conversation.getUsers({
-						where: {
-							id: socket.request.session.passport.user
-						}
-					}).then(function (users) {
-						if (users != null && users.length > 0) {
-							var messageCreationData = {};
-
-							switch(data.type) {
-								case 'image':
-									messageCreationData = {
-										type: 'image',
-										imageData: [sanitizeHtml(data.imageData, {
-											allowedTags: [],
-											allowedAttributes: []
-										})],
-										ConversationId: parseInt(data.conversationID),
-										UserId: parseInt(socket.request.session.passport.user)
-									};
-									break;
-								case 'text':
-								case 'meta':
-									messageCreationData = {
-										type: data.type,
-										textData: sanitizeHtml(data.textData),
-										ConversationId: parseInt(data.conversationID),
-										UserId: parseInt(socket.request.session.passport.user)
-									};
-									break;
-							}
-
-							objects.models.Message.create(messageCreationData).then(function (message) {
-								conversation.lastMessage = Date.now();
-								conversation.save();
-
-								socket.broadcast.to('conversation' + data.conversationID.toString())
-									.emit('newMessage', message);
-							});
-						}
-					});
-				});
+                appMessage.create(objects, socket, data.conversationID, data.messageData, data.type);
 			} else {
 				// TODO: Handle socket non-auth error
 			}
