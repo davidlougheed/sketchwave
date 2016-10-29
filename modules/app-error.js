cb3e872377685438a7a32324b98e3f7ec0721cda
David Lougheed <david.lougheed@gmail.com>
2016-10-29 14:20:39 -0400

    Restructure app error format to be more semantic, fix some code styling issues.

--- modules/app-error.js
+++ modules/app-error.js
@@ -1,17 +1,27 @@
 'use strict';
 
-module.exports.errors = {
-    403: 'forbidden',
-    404: 'notFound'
-};
+module.exports.ERROR_BAD_REQUEST = 400;
+module.exports.ERROR_FORBIDDEN = 403;
+module.exports.ERROR_NOT_FOUND = 404;
+module.exports.ERROR_INTERNAL_SERVER = 500;
+
+var errors = {};
+
+errors[module.exports.ERROR_BAD_REQUEST] = 'badRequest';
+errors[module.exports.ERROR_FORBIDDEN] = 'forbidden';
+errors[module.exports.ERROR_NOT_FOUND] = 'notFound';
+
+errors[module.exports.ERROR_INTERNAL_SERVER] = 'internalServerError';
+
+module.exports.errors = errors;
 
 module.exports.generate = function (req, res, error, meta) {
     res.status(error);
 
-    if(req.accepts('html')) {
+    if (req.accepts('html')) {
         return res.render('errors/' + error.toString(), { meta: meta });
     }
-    if(req.accepts('json')) {
+    if (req.accepts('json')) {
         return res.send({ success: false, error: module.exports.errors[error] });
     }
 
