bec7a425c8e5f36ff5ca48c342169677decf29e5
David Lougheed <david.lougheed@gmail.com>
2017-01-19 15:36:11 -0500

    Add an optional error message to let the client know why their request failed.

--- modules/app-error.js
+++ modules/app-error.js
@@ -5,6 +5,10 @@ module.exports.ERROR_FORBIDDEN = 403;
 module.exports.ERROR_NOT_FOUND = 404;
 module.exports.ERROR_INTERNAL_SERVER = 500;
 
+module.exports.MESSAGE_INVALID_ID = 'invalidId';
+module.exports.MESSAGE_PARAMETERS_REQUIRED = 'additionalParametersRequired';
+module.exports.MESSAGE_NOT_AUTHENTICATED = 'notAuthenticated';
+
 var errors = {};
 
 errors[module.exports.ERROR_BAD_REQUEST] = 'badRequest';
@@ -15,14 +19,15 @@ errors[module.exports.ERROR_INTERNAL_SERVER] = 'internalServerError';
 
 module.exports.errors = errors;
 
-module.exports.generate = function (req, res, error, meta) {
+module.exports.generate = function (req, res, error, meta, message) {
+    message = message || '';
     res.status(error);
 
     if (req.accepts('html')) {
-        return res.render('errors/' + error.toString(), { meta: meta });
+        return res.render('errors/' + error.toString(), { meta: meta, errorMessage: message });
     }
     if (req.accepts('json')) {
-        return res.send({ success: false, error: module.exports.errors[error] });
+        return res.send({ success: false, error: module.exports.errors[error], message: message });
     }
 
     return res.type('txt').send(module.exports.errors[error]);
